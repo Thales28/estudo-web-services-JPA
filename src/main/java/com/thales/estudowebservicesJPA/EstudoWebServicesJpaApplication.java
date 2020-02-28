b3387cdedad93e50522936877f451f6e15b77f11
Thales Monteiro <trm3@cin.ufpe.br>
2020-02-28 16:45:52 -0300

    Projeto criado

---
+++ src/main/java/com/thales/estudowebservicesJPA/EstudoWebServicesJpaApplication.java
@@ -0,0 +1,13 @@
+package com.thales.estudowebservicesJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoWebServicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoWebServicesJpaApplication.class, args);
+	}
+
+}
